60e93840af8c926b83eb9e9740213fbd685d17f1
aridai <aridai.net@outlook.jp>
2019-04-24 19:27:31 +0900

    エンティティを定義した。

---
+++ src/shuffle.ts
@@ -0,0 +1,7 @@
+import { Student } from "./entities";
+
+//  制約を考慮しつつ、学生を適当に配置する。
+export function shuffle(seed: number, students: Student[]): Student[][] {
+    //  TODO: 実装する。
+    throw new Error('Not implemented.');
+}
